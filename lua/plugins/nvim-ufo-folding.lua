ed6458ac6673cf3401bd17579878257663f8da67
Verson <przemyslaw.surma>
2025-05-06 01:02:54 +0200

    Fixed foldings.

--- lua/plugins/nvim-ufo-folding.lua
+++ lua/plugins/nvim-ufo-folding.lua
@@ -6,8 +6,42 @@ return {
 	config = function()
 		local ufo = require("ufo")
 
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		local handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
+				else
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					-- str width returned from truncate() may less than 2nd argument, need padding
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+					end
+					break
+				end
+				curWidth = curWidth + chunkWidth
+			end
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
+		end
+
 		vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
 		vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+		-- vim.keymap.set("n", "zr", ufo.toggleFold, { desc = "Toggle fold" })
 		vim.keymap.set("n", "zK", function()
 			local winid = ufo.peekFoldedLinesUnderCursor()
 			if not winid then
@@ -19,6 +53,7 @@ return {
 			provider_selector = function(bufnr, filetype, buftype)
 				return { "lsp", "indent" }
 			end,
+			fold_virt_text_handler = handler,
 		})
 	end,
 }
